6b7227b8c75baae49abe86b5c4ac4b2e779a92ad
Illia Avdeienko <Whiter4112@gmail.com>
2022-09-03 03:58:24 +0300

    Configured player camera control

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraOrbit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float lookSensitivity;
+    public float minXLook;
+    public float maxXLook;
+    public Transform camAnchor;
+
+    public bool invertXRotation;
+ 
+    private float _curXRot;
+
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles += Vector3.up * x * lookSensitivity;
+
+        if (invertXRotation)
+            _curXRot += y * lookSensitivity;
+        else
+            _curXRot -= y * lookSensitivity;
+        _curXRot = Mathf.Clamp(_curXRot, minXLook, maxXLook);
+
+        Vector3 clampedAngle = camAnchor.eulerAngles;
+        clampedAngle.x = _curXRot;
+        camAnchor.eulerAngles = clampedAngle; 
     }
 }
